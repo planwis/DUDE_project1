0dbddc5a5af9119ba043ce49af9c70d0748adc02
planwis <witch2544@hotmail.co.th>
2022-10-14 22:05:09 +0700

    fix gradle

--- DUDE_project1/Chess/src/test/java/TestCaseDUDE.java
+++ DUDE_project1/Chess/src/test/java/TestCaseDUDE.java
@@ -1,9 +1,15 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCaseDUDE {
+    @Before
+    private void setUp(){
+
+    }
+
     @Test
     public void testcase1() {
-        Assert.assertTrue(true);
+
     }
 }
